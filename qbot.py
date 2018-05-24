4f335bde4973b8cf5b5f2e47e131c45a9e1067a2
Andrea <hlr@HLR-MacBook-Pro.local>
2018-05-24 10:42:19 +0100

    removed query to gameapi.net and added direct socket connection to q3 server (protocol 68)

--- qbot.py
+++ qbot.py
@@ -21,14 +21,14 @@
 import discord
 import asyncio
 import json
-import urllib.request
+import socket
 import re
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!', description='''A simple bot to query Quake 3 servers using gameapis.net''')
+bot = commands.Bot(command_prefix='!', description='''A simple bot to query Quake 3 servers (protocol 68)''')
 
 aliases = {
-    # alias as 'alias': '1.1.1.1:1111'
+    # alias as 'a': '1.1.1.1:1111'
 }
 nameRegex = '\^+[a-z0-9]'
 
@@ -38,18 +38,20 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    await set_default_status()
 
 @bot.command()
-async def qinfo(argument: str):
-    if argument == 'help':
-        await bot.say('Usage: !qinfo <alias> or !qinfo ip:port, type !qinfo aliases to print a list of known aliases')
-        return
+async def alias():
+    aliasesOutput = 'Aliases:'
+    for key, value in aliases.items():
+        aliasesOutput = aliasesOutput + '\n' + key
+    await bot.say(aliasesOutput)
 
-    if argument == 'aliases':
-        aliasesOutput = 'Aliases:'
-        for key, value in aliases.items():
-            aliasesOutput = aliasesOutput + '\n' + key
-        await bot.say(aliasesOutput)
+@bot.command()
+async def q3(argument: str):
+
+    if argument == 'help':
+        await bot.say('Usage: !q3 <alias> or !q3 ip:port, type !alias to see a list of known aliases')
         return
 
     if argument == '127.0.0.1':
@@ -63,38 +65,90 @@ async def qinfo(argument: str):
             queryServer = value
             break
 
-    queryUrl = 'https://use.gameapis.net/quake3/query/info/' + queryServer
-    with urllib.request.urlopen(queryUrl) as url:
-        s = url.read()
-        response = json.loads(s)
-        if response:
-            name = re.sub(nameRegex, '', response['name'])
-            host = response['hostname']
-            port = str(response['port'])
-            currentMap = response['map']
-            playersNumber = str(response['players']['online']) + '/' + response['players']['max']
+    # get ip and port
+    query = queryServer.strip().split(":")
+    ip = query[0]
+    port = 27960
+    if len(query) > 1:
+        port = int(query[1])
 
-            embed = discord.Embed(title=name, description='', colour=0x3c9824)
-            embed.add_field(name='Server IP', value=host+':'+port, inline=True)
-            embed.add_field(name='Map', value=currentMap, inline=True)
+    status = await getServerStatus(ip, port)
+    if status is None:
+        await bot.say('There was an error fetching server status')
+    else:
+        serverInfo = status[0]
+        playersList = status[1]
+
+        name = re.sub(nameRegex, '', serverInfo['sv_hostname'])
+        currentMap = serverInfo['mapname']
+        playersNumber = str(len(playersList)) + '/' + serverInfo['sv_maxclients']
+
+        embed = discord.Embed(title=name, description='', colour=0x3c9824)
+        embed.add_field(name='Server IP', value=ip+':'+str(port), inline=True)
+        embed.add_field(name='Map', value=currentMap, inline=True)
+
+        if len(playersList) > 0:
             embed.add_field(name='Players', value=playersNumber, inline=True)
-
-            playersList = response['players']['list']
             players = []
             scores = []
             pings = []
 
             for p in playersList:
-                players.append(re.sub(nameRegex, '', p['name']))
-                scores.append(p['frags'])
-                pings.append(p['ping']+ 'ms')
-            
+                players.append(re.sub(nameRegex, '', p[2]))
+                scores.append(p[0])
+                pings.append(p[1]+ 'ms')
+
             embed.add_field(name='Player', value='\n'.join(players), inline=True)
             embed.add_field(name='Score', value='\n'.join(scores), inline=True)
             embed.add_field(name='Ping', value='\n'.join(pings), inline=True)
 
-            await bot.say(embed=embed)
-        else:
-            await bot.say('Invalid ip or alias')
+        await bot.say(embed=embed)
+
+async def getServerStatus(ip: str, port: int):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.connect((ip, port))
+        getStatus = bytes([0xff, 0xff, 0xff, 0xff, 0x67, 0x65, 0x74, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x0a])
+        sock.send(getStatus)
+        response = sock.recvfrom(2048) # response is (string, address) where address is (host, port) for AF_INET family
+        underline_bytes = response[0]
+        return parseResponse(underline_bytes.decode("latin_1"))
+
+    except socket.error as e:
+        print(e)
+        return None
+
+def parseResponse(response: str):
+    lines = response.split('\n')
+    lines = lines[1:] # first element is 'ÿÿÿÿstatusResponse', followed by server cfg then optionally players
+    serverVars = parseServerInfo(lines[0])
+    players = []
+    if len(lines) > 1: # there are players/bots
+        players = parsePlayers(lines[1:])
+    return (serverVars, players)
+
+def parseServerInfo(serverData: str):
+    data = serverData.split('\\')
+    data = data[1:] # first element is empty as string starts with \\
+    serverInfo = {}
+    # if len(data) % 2 == 0: # must be even number now
+    for i in range(0, len(data), 2):
+        key = data[i]
+        value = data[i+1]
+        serverInfo[key] = value
+
+    return serverInfo
+
+def parsePlayers(playersList: [str]):
+    players = []
+    for p in playersList:
+        if len(p) > 0:
+            playerData = p.split() # score ping name
+            players.append(playerData)
+    return players
+
+@bot.event
+async def set_default_status():
+    await bot.change_presence(game=discord.Game(name='noobs getting owned', type=3))
 
 bot.run('your_token')
